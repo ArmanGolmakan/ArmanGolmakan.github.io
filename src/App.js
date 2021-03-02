fb2d05c939023dfcbccdd880636487deee4d8cbd
Arman <arman.golmakan@gmail.com>
2021-03-02 17:49:06 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyNavbar from "./components/navbar/navbar.component";
+
+const App = () => {
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
+};
+
+export default App;
